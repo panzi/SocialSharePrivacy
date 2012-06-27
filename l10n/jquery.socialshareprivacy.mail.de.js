78524dba951c989972aa356fd3050514b0cfa141
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2012-06-28 01:32:52 +0200

    added mail support

---
+++ l10n/jquery.socialshareprivacy.mail.de.js
@@ -0,0 +1,18 @@
+/*
+ * jquery.socialshareprivacy.js | 2 Klicks fuer mehr Datenschutz
+ *
+ * Copiright (c) 2012 Mathias Panzenböck
+ *
+ * is released under the MIT License http://www.opensource.org/licenses/mit-license.php
+ *
+ * Spread the word, link to us if you can.
+ */
+
+(function ($, undefined) {
+	"use strict";
+
+	$.extend($.fn.socialSharePrivacy.settings.services.mail, {
+		'txt_info'   : 'Per E-Mail an einen Freund senden.',
+		'txt_button' : 'Sende E-Mail'
+	});
+})(jQuery);
