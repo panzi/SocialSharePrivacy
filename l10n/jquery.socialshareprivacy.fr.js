1da327c134c71b24f519314052289b0518fc06af
Michel Weimerskirch <michel@weimerskirch.net>
2012-12-17 19:17:07 +0100

    Added French translations. Images and info page are not done yet.

---
+++ l10n/jquery.socialshareprivacy.fr.js
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) 2012 Mathias Panzenböck
+ * released under the MIT License http://www.opensource.org/licenses/mit-license.php
+ */
+
+(function ($, undefined) {
+	"use strict";
+
+	$.extend($.fn.socialSharePrivacy.settings, {
+		'info_link'         : 'http://www.heise.de/ct/artikel/2-Klicks-fuer-mehr-Datenschutz-1333879.html', //TODO
+		'txt_settings'      : 'Param&eacute;tres',
+		'txt_help'          : 'D&eacute;s que vous activez ces champs en cliquant dessus, des informations seront transf&eacute;r&eacute;s &agrave; Facebook, Twitter, Google, etc aux &Eacute;tats-Unis et peuvent y &ecirc;tre stock&eacute;s. Pour plus de d&eacute;tails, cliquez sur le <em>i</em>.',
+		'settings_perma'    : 'Activer en permanence et accepter la transmission de donn&eacute;es:',
+		'language'          : 'fr'
+	});
+})(jQuery);
---
+++ l10n/jquery.socialshareprivacy.gplus.fr.js
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) 2012 Mathias Panzenböck
+ * released under the MIT License http://www.opensource.org/licenses/mit-license.php
+ */
+
+(function ($, undefined) {
+	"use strict";
+
+	$.extend($.fn.socialSharePrivacy.settings.services.gplus, {
+		'txt_info' : '2 clics pour une protection de donn&eacute;es am&eacute;lior&eacute;e: Ce n\'est qu\'apr&egrave;s avoir cliqu&eacute; ici que le bouton sera actif et vous pourrez envoyer votre recommandation &agrave; Google+. D&eacute;j&agrave; lors de l\'activation des donn&eacute;es seront transmis &agrave; des tiers &ndash; voir <em>i</em>.',
+		'txt_off'  : 'pas connect&eacute; &agrave; Google+',
+		'txt_on'   : 'connect&eacute; &agrave; Google+'
+	});
+})(jQuery);
