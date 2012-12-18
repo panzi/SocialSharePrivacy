1cfac3943a71b65e9ed6f6b45ae407cae571d71c
Michel Weimerskirch <michel@weimerskirch.net>
2012-12-18 14:43:43 +0100

    Added French images for Facebook and Twitter and removed "info_link" so it defaults to the English default

--- l10n/jquery.socialshareprivacy.twitter.fr.js
+++ l10n/jquery.socialshareprivacy.twitter.fr.js
@@ -7,6 +7,7 @@
 	"use strict";
 
 	$.extend($.fn.socialSharePrivacy.settings.services.twitter, {
+		'dummy_line_img' : 'socialshareprivacy/images/dummy_twitter_fr.png',
 		'txt_info' : '2 clics pour une protection de donn&eacute;es am&eacute;lior&eacute;e: Ce n\'est qu\'apr&egrave;s avoir cliqu&eacute; ici que le bouton sera actif et vous pourrez envoyer votre recommandation &agrave; Twitter. D&eacute;j&agrave; lors de l\'activation des donn&eacute;es seront transmis &agrave; des tiers &ndash; voir <em>i</em>.',
 		'txt_off'  : 'pas connect&eacute; &agrave; Twitter',
 		'txt_on'   : 'connect&eacute; &agrave; Twitter'
