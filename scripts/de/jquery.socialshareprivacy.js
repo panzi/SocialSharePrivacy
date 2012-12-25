a6c4c987ca3099b8d39cc43f056b780774aa4e40
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2012-12-25 22:08:21 +0100

    updated german info link

--- scripts/de/jquery.socialshareprivacy.js
+++ scripts/de/jquery.socialshareprivacy.js
@@ -15,7 +15,7 @@
  */
 
 jQuery.extend(jQuery.fn.socialSharePrivacy.settings, {
-	'info_link'         : 'http://www.heise.de/ct/artikel/2-Klicks-fuer-mehr-Datenschutz-1333879.html',
+	'info_link'         : 'http://panzi.github.com/SocialSharePrivacy/index.de.html',
 	'txt_settings'      : 'Einstellungen',
 	'txt_help'          : 'Wenn Sie diese Felder durch einen Klick aktivieren, werden Informationen an Facebook, Twitter oder Google etc. in die USA &uuml;bertragen und unter Umst&auml;nden auch dort gespeichert. N&auml;heres erfahren Sie durch einen Klick auf das <em>i</em>.',
 	'settings_perma'    : 'Dauerhaft aktivieren und Daten&uuml;ber&shy;tragung zustimmen:',
