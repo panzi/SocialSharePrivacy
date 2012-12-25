a9f813d832f814fd91eb7cb97a9ec47e8c3472d2
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2012-12-25 22:19:31 +0100

    typo and set disqus_url to canonical url.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,7 @@
 $.fn.socialSharePrivacy.settings.order = ['facebook','twitter','gplus','disqus','stumbleupon','delicious','reddit','pinterest','tumblr','linkedin','buffer','xing','mail'];
 $.fn.socialSharePrivacy.settings.services.flattr.status = false;
 var disqus_shortname = 'socialshareprivacy';
+var disqus_url = $.fn.socialSharePrivacy.settings.uri($.fn.socialSharePrivacy.settings);
 $(document).ready(function () {
 	$('#share').socialSharePrivacy({
 		layout:'box',
