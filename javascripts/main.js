bb3822617aad2a968d5200e473a35e4e79366fab
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2012-12-25 19:28:34 +0100

    gh-pages: german translation

---
+++ javascripts/main.js
@@ -0,0 +1,20 @@
+$.fn.socialSharePrivacy.settings.order = ['facebook','twitter','gplus','disqus','stumbleupon','delicious','reddit','pinterest','tumblr','linkedin','buffer','xing','mail'];
+$.fn.socialSharePrivacy.settings.services.flattr.status = false;
+var disqus_shortname = 'socialshareprivacy';
+$(document).ready(function () {
+	$('#share').socialSharePrivacy({
+		layout:'box',
+		services: {
+			disqus: {
+				shortname: disqus_shortname,
+				onclick: function () {
+					$('#comments-button').click();
+				}
+			}
+		}
+	});
+	$('#comments-button').click(function () {
+		$.getScript('http://' + disqus_shortname + '.disqus.com/embed.js');
+		$(this).remove();
+	});
+});
