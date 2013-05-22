a463a2ccee03aa2429d3794f9008b02bfe90bc14
pzel <p@manisola.net>
2013-05-22 19:13:24 +0200

    Polish translation

---
+++ scripts/pl/jquery.socialshareprivacy.facebook.js
@@ -0,0 +1,24 @@
+/*
+ * jquery.socialshareprivacy.js | 2 Klicks fuer mehr Datenschutz
+ *
+ * http://www.heise.de/extras/socialshareprivacy/
+ * http://www.heise.de/ct/artikel/2-Klicks-fuer-mehr-Datenschutz-1333879.html
+ *
+ * Copyright (c) 2011 Hilko Holweg, Sebastian Hilbig, Nicolas Heiringhoff, Juergen Schmidt,
+ * Heise Zeitschriften Verlag GmbH & Co. KG, http://www.heise.de
+ *
+ * Copyright (c) 2012 Mathias Panzenböck
+ * Polish translation (c) 2013 Simon Zelazny
+ *
+ * is released under the MIT License http://www.opensource.org/licenses/mit-license.php
+ *
+ * Spread the word, link to us if you can.
+ */
+
+jQuery.extend(jQuery.fn.socialSharePrivacy.settings.services.facebook, {
+	'dummy_line_img' : 'images/de/dummy_facebook.png',
+	'dummy_box_img'  : 'images/de/dummy_box_facebook.png',
+  'txt_info' : 'Dwa kliknięcia &ndash; więcej prywatności: Kliknięcie tutaj uruchomi przycisk "Lubię to". Samo uchuchomienie przycisku powoduje wysłanie Twoich danych do Facebooka. Kliknij w <em>i</em>, aby dowiedzieć się więcej.',
+	'txt_off'  : 'brak połączenia z Facebookiem',
+	'txt_on'   : 'połączono z Facebookiem'
+});
